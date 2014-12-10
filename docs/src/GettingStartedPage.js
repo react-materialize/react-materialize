3cf36074edbcab009defc0801e2409205062114c
Isaiah Peng <issaria@gmail.com>
2014-12-10 18:00:24 +0100

    add travis

--- docs/src/GettingStartedPage.js
+++ docs/src/GettingStartedPage.js
@@ -8,10 +8,10 @@ var GettingStartedPage = React.createClass({
   render() {
     var shims = '<!--[if lt IE 9]>\n' +
         '<script>\n' +
-          '(function(){\n' +
-            'var ef = function(){};\n' +
-            'window.console = window.console || {log:ef,warn:ef,error:ef,dir:ef};\n' +
-          '}());\n' +
+        '  (function(){\n' +
+        '    var ef = function(){};\n' +
+        '    window.console = window.console || {log:ef,warn:ef,error:ef,dir:ef};\n' +
+        '  }());\n' +
         '</script>\n' +
         '<script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script>\n' +
         '<script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv-printshiv.min.js"></script>\n' +
@@ -24,7 +24,7 @@ var GettingStartedPage = React.createClass({
         <Col s={12}>
           <h4>Set Up</h4>
           <pre>
-            <code>
+            <code className='language-markup'>
               $ npm install react-materialize
             </code>
           </pre>
@@ -32,7 +32,7 @@ var GettingStartedPage = React.createClass({
         <Col s={12}>
           <h4>Browser Support</h4>
           <pre>
-            <code className='html'>
+            <code className='language-markup'>
               {shims}
             </code>
           </pre>
