05af557b5bd3080bd371f4a94fdf601d97bb0e08
Isaiah Peng <issaria@gmail.com>
2015-10-30 23:25:16 +0100

    fix table component

--- src/Table.js
+++ src/Table.js
@@ -2,26 +2,31 @@ import React from 'react';
 import cx from 'classnames';
 
 class Table extends React.Component {
-  render() {
-    var classes = {
-      centered: this.props.centered,
-      hoverable: this.props.hoverable,
-      stripped: this.props.stripped,
-      bordered: this.props.bordered
-    };
-    return (
-      <table className={cx(this.props.className, classes)} {...this.props}>
-        {this.props.children}
-      </table>
-    );
-  }
+    render() {
+        var classes = {
+            centered: this.props.centered,
+            highlight: this.props.hoverable,
+            "responsive-table": this.props.responsive,
+            stripped: this.props.stripped,
+            bordered: this.props.bordered
+        };
+
+        var {className, children, ...props} = this.props;
+
+        return (
+            <table className={cx(classes, className)} {...this.props}>
+                {children}
+            </table>
+        );
+    }
 }
 
 Table.propTypes = {
     centered: React.PropTypes.bool,
     hoverable: React.PropTypes.bool,
+    responsive: React.PropTypes.bool,
     stripped: React.PropTypes.bool,
     bordered: React.PropTypes.bool
-}
+};
 
 export default Table;
