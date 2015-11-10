e97c6c743e9e9cbf0ad748b1c0c8b9b2223ee671
Isaiah Peng <issaria@gmail.com>
2015-11-10 18:03:58 +0100

    convert badge text prop to a bool

--- src/Badge.js
+++ src/Badge.js
@@ -1,23 +1,25 @@
 import React from 'react';
+import cx from 'classnames';
 
 class Badge extends React.Component {
-  render() {
-    let classes = ['badge'];
-    if (this.props.text) {
-      classes.push(this.props.text);
-      classes = classes.reverse();
-    }
+    render() {
+        let {className, newIcon, children, ...props} = this.props;
+        let classes = {
+            badge: true,
+            "new": newIcon
+        };
 
-    return (
-      <span className={classes.join(' ')}>
-        {this.props.children}
-      </span>
-    );
-  }
+        return (
+            <span className={cx(classes)} {...props}>
+                {children}
+            </span>
+        );
+    }
 }
 
 Badge.propTypes = {
-  text: React.PropTypes.string
+    // add the "new" class name
+    newIcon: React.PropTypes.bool
 };
 
 export default Badge;
