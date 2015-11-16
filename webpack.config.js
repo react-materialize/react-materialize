4b0625fe5a1b14096ad4d317c1b5a679643843cd
Isaiah Peng <issaria@gmail.com>
2015-11-16 12:37:44 +0100

    fix npm package, release 0.14.0-beta2

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,40 @@
 var path = require('path');
 var webpack = require('webpack');
 module.exports = {
-    entry: [
-      "./src/index.js"
-    ],
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: "react-materialize.js"
-    },
-    module: {
-        loaders: [
-            { test: /\.js$/, exclude: /node_modules/, loader: "babel", query: {stage: 0}}
-        ]
-    },
-    plugins: [
-      new webpack.HotModuleReplacementPlugin(),
-      new webpack.NoErrorsPlugin()
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'index.js',
+    library: 'ReactMaterialize',
+    libraryTarget: 'umd'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel', query: {stage: 0}}
     ]
-
+  },
+  externals: [
+    {
+      'react': {
+        root: 'React',
+        commonjs2: 'react',
+        commonjs: 'react',
+        amd: 'react'
+      }
+    },
+    {
+      'react-dom': {
+        root: 'ReactDOM',
+        commonjs2: 'react-dom',
+        commonjs: 'react-dom',
+        amd: 'react-dom'
+      }
+    }
+  ],
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ]
 };
