8e27d4acf67c8f6b49a604ac5a2f1072e43019df
Alexander Odell <alextrastero@gmail.com>
2016-01-19 23:29:31 +0100

    Configure stylesheet

--- docs/webpack.config.js
+++ docs/webpack.config.js
@@ -1,13 +1,21 @@
 var path = require('path');
 var yargs = require('yargs');
+var autoprefixer = require('autoprefixer');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const sassLoaders = [
+  'css-loader',
+  'postcss-loader',
+  'sass-loader?indentedSyntax=sass&includePaths[]=' + path.resolve(__dirname, './src')
+]
 
 var options = yargs
-      .alias('d', 'debug')
-      .option('port', {
-        default: '8080',
-        type: 'string'
-      })
-      .argv;
+  .alias('d', 'debug')
+  .option('port', {
+    default: '8080',
+    type: 'string'
+  })
+  .argv;
 
 var webpackDevServerAddr = 'http://localhost:' + options.port;
 
@@ -16,9 +24,20 @@ module.exports = {
     './client.js'
   ],
   output: {
-    publicPath: options.debug ? webpackDevServerAddr + '/assets/' : '/assets/',
-    filename: 'bundle.js',
-    path: './assets'
+    publicPath: options.debug ? webpackDevServerAddr + '/assets/' : '/assets/', //publicPath: '/build'
+    filename: 'bundle.js', //filename: '[name].js',
+    path: './assets' //path: path.join(__dirname, './build'),
+  },
+  plugins: [
+    new ExtractTextPlugin('[name].css')
+  ],
+  postcss: [
+    autoprefixer({
+      browsers: ['last 2 versions']
+    })
+  ],
+  resolve: {
+    extensions: ['', '.js', '.scss']
   },
   module: {
     loaders: [
