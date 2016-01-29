e01347fd37736dfe7e1333c827869eedd64ec7a1
Isaiah Peng <issaria@gmail.com>
2016-01-29 22:42:38 +0100

    fix preloader spinner

--- src/Preloader.js
+++ src/Preloader.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import cx from 'classnames';
 
-const Spinner = () => {
-  let {color, only} = this.props;
+const Spinner = (props) => {
+  let {color, only} = props;
   let spinnerClasses = {
     'spinner-layer': true
   };
