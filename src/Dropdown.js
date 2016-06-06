b674e58fbf5065e12d67f844f46d33257ac79243
Alexander Odell <alextrastero@gmail.com>
2016-06-06 20:17:04 +0200

    Dropdown Component - will initialize when mounted
     - Also changed overorigin to beloworigin by default
     - You can now pass options hash with dropdown options
     - Listener will be removed on unmount

--- src/Dropdown.js
+++ src/Dropdown.js
@@ -1,34 +1,45 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
 import idgen from './idgen';
 
-class Dropdown extends React.Component {
+class Dropdown extends Component {
   constructor(props) {
     super(props);
     this.renderTrigger = this.renderTrigger.bind(this);
   }
 
-  render() {
-    let {trigger, children, ...props} = this.props;
-    let idx = 'dropdown_' + idgen();
-    return (
-      <span>
-        {this.renderTrigger(idx)}
-        <ul className='dropdown-content' id={idx}>
-          {children}
-        </ul>
-      </span>
-    )
+  componentDidMount() {
+    const options = this.props.options || {};
+
+    $(this._trigger).dropdown(options);
+  }
+ 
+  componentWillUnmount() {
+    $(this._trigger).off();
   }
 
-  renderTrigger(idx) {
-    let trigger = this.props.trigger;
-    return React.cloneElement(trigger,
-      {
-        className: 'dropdown-button',
-        'data-beloworigin': true,
-        'data-activates': idx
-      }
+  render() {
+    const { children, ...props } = this.props;
+    this.idx = 'dropdown_' + idgen();
+
+    return (
+      <span>
+        { this.renderTrigger() }
+        <ul {...props} className='dropdown-content' id={ this.idx }>
+          { children }
+        </ul>
+      </span>
     );
+  }
+
+  renderTrigger() {
+    const { trigger, beloworigin } = this.props;
+
+    return React.cloneElement( trigger, {
+      ref: (t) => this._trigger = t,
+      className: 'dropdown-button',
+      'data-beloworigin': beloworigin,
+      'data-activates': this.idx
+    });
   }
 }
 
@@ -36,15 +47,20 @@ Dropdown.propTypes = {
   /**
    * The button to trigger the dropdown
    */
-  trigger: React.PropTypes.node.isRequired,
+  trigger: PropTypes.node.isRequired,
   /**
-   * 	If true, the dropdown will show over the activator. Default: false
+   * 	If true, the dropdown will show below the activator. Default: true
    */
-  overorigin: React.PropTypes.bool,
+  beloworigin: PropTypes.bool,
+  children: PropTypes.node,
+  /**
+   * Options hash for the dropdown
+   */
+  options: PropTypes.object
 };
 
 Dropdown.defaultProps = {
-  overorigin: false,
-}
+  beloworigin: true
+};
 
 export default Dropdown;
