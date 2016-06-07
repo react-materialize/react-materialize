9e2f3486143855a3f9b6e1bb0faabade13e4a5a8
Alexander Odell <alextrastero@gmail.com>
2016-06-07 09:07:16 +0200

    Add initialise after mount

--- src/Tabs.js
+++ src/Tabs.js
@@ -1,43 +1,74 @@
 import React from 'react';
 import cx from 'classnames';
+import idgen from './idgen';
 
 import Row from './Row';
 import Col from './Col';
 
 class Tabs extends React.Component {
+  componentDidMount() {
+    if (typeof $ !== 'undefined') {
+      $(this.refs[this.tabsIdx]).tabs();
+    }
+  }
+
+  renderChildren() {
+    return (
+      React.Children.map(this.props.children, (child, idx) => {
+        const {
+          title,
+          tabWidth,
+          className,
+          active,
+          disabled
+        } = child.props;
+
+        let classes = {
+          tab: true,
+          disabled,
+          col: true
+        };
+        if (tabWidth) { classes['s' + tabWidth] = true; }
+        const target = '#tab_' + idx;
+
+        return (
+          <li className={cx(classes, className)} key={idx}>
+            <a href={target} className={active ? 'active' : ''}>{title}</a>
+          </li>
+        );
+      })
+    );
+  }
+
+  renderGrandChildren() {
+    return (
+      React.Children.map(this.props.children, (child, idx) => {
+        return (
+          <Col id={'tab_' + idx} s={12} key={'tab' + idx}>
+            { child.props.children }
+          </Col>
+        );
+      })
+    );
+  }
+
   render() {
-    let {children, className, ...props} = this.props;
+    const {
+      children,
+      className,
+      ...props
+    } = this.props;
+
+    this.tabsIdx = `tabs_${idgen()}`;
+
     return (
       <Row>
         <Col s={12}>
-          <ul className={cx('tabs', className)}>
-            {
-              React.Children.map(children, (child, idx) => {
-                let {title, tabWidth, className, active, disabled} = child.props;
-                //if (!tabWidth) {
-                //  tabWidth = Math.floor(12 / count);
-                //}
-                let classes = {
-                  tab: true,
-                  disabled,
-                  col: true
-                };
-                if (tabWidth) classes['s' + tabWidth] = true;
-                let target = '#tab_' + idx;
-                return (
-                  <li className={cx(classes, className)} key={idx}>
-                    <a href={target} className={active ? 'active' : ''}>{title}</a>
-                  </li>
-                );
-              })
-            }
+          <ul ref={this.tabsIdx} className={cx('tabs', className)}>
+            { this.renderChildren() }
           </ul>
         </Col>
-        {
-          React.Children.map(children, (child, idx) => {
-            return <Col id={'tab_' + idx} s={12} key={'tab' + idx}>{child.props.children}</Col>;
-          })
-        }
+        { this.renderGrandChildren() }
       </Row>
     );
   }
