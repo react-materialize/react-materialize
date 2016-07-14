4e125232c938ebea5b05e29f38203d64d412b7e2
Alexander Odell <alextrastero@gmail.com>
2016-07-14 17:45:11 +0200

    Modify components to not show the react 15 warning message  - Breadcrumb  - Col

--- src/Col.js
+++ src/Col.js
@@ -3,56 +3,60 @@ import cx from 'classnames';
 
 import constants from './constants';
 
-class Col extends React.Component {
-  render() {
-    let {node, offset, className, children, ...props} = this.props;
-    let C = node;
-    let classes = {col: true};
-    constants.SIZES.forEach(s => {
-      if (this.props[s]) {
-        classes[s + this.props[s]] = true;
-      }
-    });
-
-    if (offset) {
-      offset.split(' ').forEach(off => {
-        classes['offset-' + off] = true;
-      });
+const Col = ({
+  children,
+  className,
+  node = 'div',
+  offset,
+  ...props
+}) => {
+  let Child = node;
+  let classes = { col: true };
+  constants.SIZES.forEach(s => {
+    if (props[s]) {
+      classes[s + props[s]] = true;
     }
-    return (
-      <C {...props} className={cx(classes, className)}>
-        {children}
-      </C>
-    );
+  });
+
+  if (offset) {
+    offset.split(' ').forEach(off => {
+      classes['offset-' + off] = true;
+    });
   }
-}
+
+  return (
+    <Child className={cx(classes, className)}>
+      {children}
+    </Child>
+  );
+};
 
 Col.propTypes = {
-  /**
-   * The node to be used for the column
-   * @default div
-   */
-  node: React.PropTypes.node.isRequired,
-  /**
-   * Columns for small size screens
-   */
-  s: React.PropTypes.number,
-  /**
-   * Columns for middle size screens
-   */
-  m: React.PropTypes.number,
+  children: React.PropTypes.node,
+  className: React.PropTypes.string,
   /**
    * Columns for large size screens
    */
   l: React.PropTypes.number,
   /**
+   * Columns for middle size screens
+   */
+  m: React.PropTypes.number,
+  /**
+   * The node to be used for the column
+   * @default div
+   */
+  node: React.PropTypes.node,
+  /**
    * To offset, simply add s2 to the class where s signifies the screen
    * class-prefix (s = small, m = medium, l = large) and the number after
    * is the number of columns you want to offset by.
    */
-  offset: React.PropTypes.string
-}
-
-Col.defaultProps = {node: 'div'};
+  offset: React.PropTypes.string,
+  /**
+   * Columns for small size screens
+   */
+  s: React.PropTypes.number
+};
 
 export default Col;
