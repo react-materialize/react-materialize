0122bc2ae71a30443296c2b700fbc208034b11d9
Alexander Garth Odell <alextrastero@gmail.com>
2016-08-16 15:01:26 +0200

    Add className functionality to progress bar (#99)
    * Add className functionality to progress bar
    
    * Stateless 😤

--- src/ProgressBar.js
+++ src/ProgressBar.js
@@ -1,33 +1,33 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
 import cx from 'classnames';
 
-class ProgressBar extends React.Component {
-  render() {
-    let classes, style;
-    if(typeof this.props.progress === 'undefined') {
-      classes = {indeterminate: true};
-    } else {
-      classes = {determinate: true};
-      style = {
-        width: this.props.progress + '%',
-      };
-    }
-
-    return (
-      <div className={cx('progress', this.props.className)}>
-        <div className={cx(classes)} style={style}></div>
-      </div>
-    )
+const ProgressBar = ({ progress, className }) => {
+  let style;
+  const classes = {
+    indeterminate: !progress,
+    determinate: !!progress
+  };
+  if (progress) {
+    style = {
+      width: progress + '%'
+    };
   }
-}
+
+  return (
+    <div className={cx('progress lighten-5', className)}>
+      <div className={cx(classes, className)} style={style} />
+    </div>
+  );
+};
 
 ProgressBar.propTypes = {
+  className: PropTypes.string,
   /**
    * A number between 0..100 that indicates the current progress,
    * when provided, a determinate bar with the progress is displayed,
    * otherwise a indeterminate bar is shown
    */
-  progress: React.PropTypes.number,
+  progress: PropTypes.number
 };
 
 export default ProgressBar;
