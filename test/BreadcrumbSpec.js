391ca045552215859347f24553d5a10f6a46ade6
Alexander Odell <alextrastero@gmail.com>
2016-09-01 12:38:40 +0200

    Add Breadcrumb spec

--- test/BreadcrumbSpec.js
+++ test/BreadcrumbSpec.js
@@ -1,19 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import TestUtils from 'react-addons-test-utils';
-import Breadcrumb from '../src/Breadcrumb';
-import MenuItem from '../src/MenuItem';
+/* global describe, it, expect */
 
-describe('Breadcrumb', () => {
-  it('should create nav element', () => {
-    const instance = TestUtils.renderIntoDocument(
-      <Breadcrumb>
-        <MenuItem>first</MenuItem>
-        <MenuItem>first</MenuItem>
-        <MenuItem>first</MenuItem>
-      </Breadcrumb>
-    );
-    const nav = TestUtils.findRenderedDOMComponentWithTag(instance, 'nav');
-    assert.equal(ReactDOM.findDOMNode(nav).className, 'row');
+import React from 'react';
+import { shallow } from 'enzyme';
+import Breadcrumb from '../src/Breadcrumb';
+
+const wrapper = shallow(<Breadcrumb />);
+
+describe('<Breadcrumb />', () => {
+  it('renders', () => {
+    expect(wrapper).to.have.length(1);
   });
 });
