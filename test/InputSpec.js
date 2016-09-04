9b215f8f158ba7f936df79114d708e8e123c0707
Alexander Odell <alextrastero@gmail.com>
2016-09-04 11:27:12 +0200

    Add/refactor Input spec

--- test/InputSpec.js
+++ test/InputSpec.js
@@ -1,55 +1,58 @@
+/* global describe, it, expect */
+
 import React from 'react';
-import ReactDOM from 'react-dom';
-import ReactDOMServer from 'react-dom/server';
-import TestUtils from 'react-addons-test-utils';
+import { shallow, mount } from 'enzyme';
+
 import Input from '../src/Input';
 import Icon from '../src/Icon';
 
-describe('Input', function() {
-  it('renders a text field by default', function() {
-    let instance = TestUtils.renderIntoDocument(<Input />);
-    let input = TestUtils.findRenderedDOMComponentWithTag(instance, 'input');
-    input = ReactDOM.findDOMNode(input);
-    assert.equal(input.nodeName, 'INPUT');
-    assert.equal(input.getAttribute('type'), 'text');
+describe('<Input />', () => {
+  let wrapper = shallow(<Input />);
+
+  it('renders a text field by default', () => {
+    expect(wrapper.find('.input-field')).to.have.length(1);
   });
 
-  it('renders a textarea when type is textarea', function() {
-    let instance = TestUtils.renderIntoDocument(<Input type='textarea'/>);
-    let textarea = TestUtils.findRenderedDOMComponentWithTag(instance, 'textarea');
-    assert.ok(textarea);
+  it('renders a textarea when type is textarea', () => {
+    wrapper = shallow(<Input type='textarea' />);
+    expect(wrapper.find('textarea')).to.have.length(1);
   });
 
-  it('renders a select element when type is select', function() {
-    let instance = TestUtils.renderIntoDocument(
+  it('renders a select element when type is select', () => {
+    wrapper = mount(
       <Input type='select' defaultValue='v'>
-        <option value='v'/>
-        <option value='w'/>
+        <option value='v' />
+        <option value='w' />
       </Input>);
-    let select = TestUtils.findRenderedDOMComponentWithTag(instance, 'select');
-    assert.equal(ReactDOM.findDOMNode(select).nodeName, 'SELECT');
+    expect(wrapper.find('select')).to.have.length(1);
   });
 
   it('renders an icon with prefix class name when icon prefix is used', () => {
-    let instance = TestUtils.renderIntoDocument(
+    wrapper = mount(
       <Input type='text'><Icon>account_circle</Icon></Input>
     );
-    let input = TestUtils.findRenderedDOMComponentWithTag(instance, 'i');
-    assert.include(ReactDOM.findDOMNode(input).className, 'prefix');
+    expect(wrapper.find('i.prefix')).to.have.length(1);
   });
 
   it('renders placeholder as the default disabled option for select', () => {
     let defaultOption = 'Choose your option';
-    let instance = TestUtils.renderIntoDocument(
-        <Input type='select' placeholder={defaultOption}>
-            <option value='1'>Option 1</option>
-            <option value='2'>Option 2</option>
-            <option value='3'>Option 3</option>
-        </Input>);
-    let options = TestUtils.scryRenderedDOMComponentsWithTag(instance, 'option');
-    assert.ok(options);
-    assert.equal(options.length, 4);
-    assert.equal(options[0].getAttribute('disabled'), '');
-    assert.equal(options[0].value, defaultOption);
+    wrapper = mount(
+      <Input type='select' placeholder={defaultOption}>
+        <option value='1'>Option 1</option>
+        <option value='2'>Option 2</option>
+        <option value='3'>Option 3</option>
+      </Input>);
+    expect(wrapper.find('option')).to.have.length(4);
+  });
+
+  it('renders a select with multiple', () => {
+    let defaultOption = 'Choose your option';
+    wrapper = mount(
+      <Input type='select' multiple placeholder={defaultOption}>
+        <option value='1'>Option 1</option>
+        <option value='2'>Option 2</option>
+        <option value='3'>Option 3</option>
+      </Input>);
+    expect(wrapper.find('select[multiple]')).to.have.length(1);
   });
 });
