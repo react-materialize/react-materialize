0b5959eab55347d52f17cf29b4cda6e55e5272de
Alexander Odell <alextrastero@gmail.com>
2016-09-05 11:45:24 +0200

    Add Slider and Slide spec

---
+++ test/SliderSpec.js
@@ -0,0 +1,14 @@
+/* global describe, it */
+
+import React from 'react';
+import { shallow } from 'enzyme';
+import { assert } from 'chai';
+import Slider from '../src/Slider';
+
+const wrapper = shallow(<Slider />);
+
+describe('<Slider />', () => {
+  it('should render a Slider', () => {
+    assert.ok(wrapper.find('.slider').length, 'should render a slider');
+  });
+});
