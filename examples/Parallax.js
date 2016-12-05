72ac4537a9a2f073caad48d4266ea5a36e226cd6
Junio Branco <juniob.dw@gmail.com>
2016-12-05 21:45:14 +0800

    Parallax Component. (#192)
    * Implement Parallax component

---
+++ examples/Parallax.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Parallax from '../src/Parallax';
+
+export default
+<div>
+  <Parallax imgSrc="http://materializecss.com/images/parallax1.jpg"/>
+  <div class="section white">
+    <div class="row container">
+      <h2 class="header">Parallax</h2>
+      <p class="grey-text text-darken-3 lighten-3">Parallax is an effect where the background content or image in this case, is moved at a different speed than the foreground content while scrolling.</p>
+    </div>
+  </div>
+  <Parallax imgSrc="http://materializecss.com/images/parallax2.jpg"/>
+</div>;
