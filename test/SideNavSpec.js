6cbb463dfd1cb8c1ddc2e7c868cb0dc9e34320ba
Richard Meijer <richard.meijer@gmail.com>
2017-01-03 20:59:28 +0100

    Added basic first test

--- test/SideNavSpec.js
+++ test/SideNavSpec.js
@@ -4,11 +4,25 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { assert } from 'chai';
 import SideNav from '../src/SideNav';
+import Button from '../src/Button';
 
-let wrapper = shallow(<SideNav />);
+function setup (props = {}, mounted) {
+  const propsIn = {
+    trigger: <Button>Show sidenav</Button>,
+    ...props
+  };
+  const wrapper = shallow(<SideNav {...propsIn} />);
+  const propsOut = wrapper.props();
+  return {
+    propsIn,
+    wrapper,
+    propsOut
+  };
+}
 
 describe('<SideNav />', () => {
   it('should render', () => {
-    assert(wrapper.find('nav').length);
+    const { wrapper } = setup();
+    assert(wrapper.find('.side-nav').length, 'should render a sidenav');
   });
 });
