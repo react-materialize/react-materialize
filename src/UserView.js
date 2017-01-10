752c930feea8fcdf0e7d8152fa0dd02270ceaa91
Richard Meijer <richard.meijer@gmail.com>
2017-01-10 13:40:18 +0100

    separated userview

---
+++ src/UserView.js
@@ -0,0 +1,42 @@
+import React, { Component, PropTypes } from 'react';
+
+export const UserShape = {
+  background: PropTypes.string,
+  image: PropTypes.string,
+  name: PropTypes.string,
+  email: PropTypes.string
+};
+
+export class UserView extends Component {
+  render () {
+    const { background, image, name, email } = this.props;
+    return (
+      <div className='userView'>
+        { background &&
+          <div className='background'>
+            <img src={background} />
+          </div>
+        }
+        { image &&
+          <a href='#!user'>
+            <img className='circle' src={image} />
+          </a>
+        }
+        { name &&
+          <a href='#!name'>
+            <span className='white-text name'>{name}</span>
+          </a>
+        }
+        { email &&
+          <a href='#!email'>
+            <span className='white-text email'>{email}</span>
+          </a>
+        }
+      </div>
+    );
+  }
+};
+
+UserView.propTypes = UserShape;
+
+export default UserView;
