9fd36f8374f9f5452a0f8e8864ba7158bba89b80
Maxim Penkov <anekdots1@yandex.ru>
2017-01-11 21:02:25 +0300

    fix: Lint fix: typo numeric => number in React.PropTypes fix: Test - removed bad rendering test cases

--- src/Tooltip.js
+++ src/Tooltip.js
@@ -1,50 +1,49 @@
 /**
  * Created by madmax on 10.01.17.
  */
-import React from "react"
-
+import React from 'react';
 
 class Tooltip extends React.Component {
-    componentDidMount() {
-        if (jQuery !== undefined) {
-            jQuery(React.findDOMNode(this._tooltipped)).tooltip();
-        }
+  componentDidMount () {
+    if (jQuery !== undefined) {
+      jQuery(React.findDOMNode(this._tooltipped)).tooltip();
     }
+  }
 
-    render() {
-        const {
-            tooltip,
-            delay,
-            position,
-            children,
-            ...newProps
+  render () {
+    const {
+          tooltip,
+          delay,
+          position,
+          children,
+          ...newProps
         } = this.props;
 
-        const tooltipProps = {
-            "data-tooltip": tooltip,
-            "data-delay": delay,
-            "data-position": position
-        };
+    const tooltipProps = {
+      'data-tooltip': tooltip,
+      'data-delay': delay,
+      'data-position': position
+    };
 
-        return React.createElement(
-            node.type,
-            { ...newProps, ...tooltipProps, "ref": (obj) => { this._tooltipped = obj; }},
-            children
+    return React.createElement(
+          children.type,
+          {...newProps, ...tooltipProps, 'ref': (obj) => { this._tooltipped = obj; }},
+          children
         );
-    }
+  }
 }
 
 Tooltip.propTypes = {
-    "children": React.PropTypes.node,
+  'children': React.PropTypes.element.isRequired,
 
-    "tooltip": React.PropTypes.string,
-    "delay": React.PropTypes.numeric,
-    "position": React.PropTypes.string
+  'tooltip': React.PropTypes.string,
+  'delay': React.PropTypes.number,
+  'position': React.PropTypes.string
 };
 
 Tooltip.defaultProps = {
-    "delay": 2000,
-    "position": "bottom"
+  'delay': 2000,
+  'position': 'bottom'
 };
 
 export default Tooltip;
--- test/TooltipSpec.js
+++ test/TooltipSpec.js
@@ -7,45 +7,28 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Tooltip from '../src/Tooltip';
 
-const child = 'hey';
-const wrapper = shallow(
-    <Tag>{child}</Tag>
-);
-
 describe('<Tooltip />', () => {
-    it("Doesn't render correctly if more than one child node presented", () => {
-        expect(function() {
-            return shallow(<Tooltip tooltip="123"><div/><div/></Tooltip>)
-        }).to.throw(Error);
-    });
+  it('Renders correctly if there is only one child node', () => {
+    shallow(<Tooltip tooltip='123'><div /></Tooltip>);
+  });
 
-    it("Doesn't render correctly if there is no child nodes", () => {
-        expect(function() {
-            return shallow(<Tooltip tooltip="123" />)
-        }).to.throw(Error);
-    });
+  it('Render itself as a modified child node', () => {
+    const wrapper = shallow(<Tooltip tooltip='123'><div /></Tooltip>);
 
-    it("Renders correctly if there is only one child node", () => {
-        shallow(<Tooltip tooltip="123"><div/></Tooltip>);
-    });
+    expect(wrapper.is('div')).to.equal(true);
+    expect(wrapper.equals(<div />)).to.equal(false);
+  });
 
-    it("Render itself as a modified child node", () => {
-        const wrapper = shallow(<Tooltip tooltip="123"><div/></Tooltip>);
+  it('Should transfer tooltip, delay, position props into the corresponding data-* equivs on the child node', () => {
+    const tooltip = '123';
+    const delay = 999;
+    const position = 'left';
 
-        expect(wrapper.is("div")).to.equal(true);
-        expect(wrapper.equals(<div/>)).to.equal(false);
-    });
+    const wrapper = shallow(<Tooltip tooltip={tooltip} delay={delay} position={position}><div /></Tooltip>);
 
-    it("Should transfer tooltip, delay, position props into the corresponding data-* equivs on the child node", () => {
-        const tooltip = "123";
-        const delay = 999;
-        const position = "left";
-
-        const wrapper = shallow(<Tooltip tooltip={tooltip} delay={delay} position={position}><div/></Tooltip>);
-
-        expect(wrapper.is("div")).to.equal(true);
-        expect(wrapper.prop("data-tooltip")).to.equal(tooltip);
-        expect(wrapper.prop("data-delay")).to.equal(delay);
-        expect(wrapper.prop("data-position")).to.equal(position);
-    });
+    expect(wrapper.is('div')).to.equal(true);
+    expect(wrapper.prop('data-tooltip')).to.equal(tooltip);
+    expect(wrapper.prop('data-delay')).to.equal(delay);
+    expect(wrapper.prop('data-position')).to.equal(position);
+  });
 });
