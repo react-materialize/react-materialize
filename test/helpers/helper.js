8dc3ddb5f7932ade3da9cfd3126159344cffb461
Alexander Odell <alextrastero@gmail.com>
2017-01-28 17:20:20 +0100

    Setup mocha jsdom

--- test/helpers/helper.js
+++ test/helpers/helper.js
@@ -1,12 +1,18 @@
 var baseDOM = '<!DOCTYPE html><html><head><meta charset="utf-8"></head><body></body></html>';
 var jsdom = require('jsdom').jsdom;
-global.document = jsdom(baseDOM);
-global.window = document.defaultView;
+
 if (global.self != null) {
   console.log(' global.self >>>>> ' + global.self);
 } else {
   global.self = global.this;
 }
+
+global.document = jsdom(baseDOM);
+global.window = document.defaultView;
+
 global.navigator = {
   userAgent: 'node.js'
 };
+
+global.expect = require('chai').expect;
+global.sinon = require('sinon');
