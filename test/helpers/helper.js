9b6dbf2be0c4bec0d1aba491edfc4617d9636676
Alexander Odell <alextrastero@gmail.com>
2017-01-29 17:41:26 +0100

    Replaces Karma + Jasmine with Mocha + jsdom (#213)
    * Move tests to spec
    
    * remove unwanted packages
    
    * move files to spec
    
    * Setup mocha jsdom
    
    * Try another alternative
    
    * Add WIP helper
    
    * Fixes materialize global
    
    * Add warning comment
    
    * renames test case

---
+++ test/helpers/helper.js
@@ -0,0 +1,32 @@
+const exposedProperties = ['window', 'navigator', 'document'];
+
+const expect = require('chai').expect;
+const jsdom = require('jsdom').jsdom;
+global.sinon = require('sinon');
+global.jsdom = jsdom.jsdom;
+global.React = require('react');
+global.ReactDOM = require('react-dom');
+global.TestUtils = require('react-addons-test-utils');
+global.expect = expect;
+
+global.document = jsdom('');
+global.window = document.defaultView;
+
+global.$ = require('jquery');
+
+// TODO not sure about this
+global.$.fn.material_select = () => this;
+global.$.fn.sideNav = () => this;
+
+Object.keys(document.defaultView).forEach(
+  (property) => {
+    if (typeof global[property] === 'undefined') {
+      exposedProperties.push(property);
+      global[property] = document.defaultView[property];
+    }
+  }
+);
+
+global.navigator = {
+  userAgent: 'Node.js'
+};
