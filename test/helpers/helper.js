e54a60f26ed83612b9f0ef8766ea63239c490759
Alexander Odell <alextrastero@gmail.com>
2017-02-18 12:03:26 +0100

    Adds modalOptions to modal plus more

--- test/helpers/helper.js
+++
@@ -1,9 +0,0 @@
-require('jsdom-global')();
-
-global.expect = require('chai').expect;
-global.sinon = require('sinon');
-
-global.$ = require('jquery');
-
-global.$.fn.material_select = () => this;
-global.$.fn.sideNav = () => this;
