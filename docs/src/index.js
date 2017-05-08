1f26548e2820c0b27e8cea39e266cc90bbae0ca2
Alexander Odell <alextrastero@gmail.com>
2017-05-08 10:48:18 +0200

    Clean up index template

--- docs/src/index.js
+++ docs/src/index.js
@@ -3,8 +3,6 @@ require('react-hot-loader/patch');
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
-// import 'materialize-css/dist/css/materialize.min.css';
-// import 'materialize-css/css/prism.css';
 import 'materialize-css/css/ghpages-materialize.css';
 import '../assets/stylesheets/main.css';
 import App from './components/App';
