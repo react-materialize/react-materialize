3921f886e8ffb49508ac0bb3aa016ccb0fe2e6f7
Alexander Odell <alextrastero@gmail.com>
2017-05-21 19:07:33 +0200

    Adds container component

---
+++ test/ContainerSpec.js
@@ -0,0 +1,20 @@
+/* global describe, it */
+
+import React from 'react';
+import { shallow } from 'enzyme';
+import { assert } from 'chai';
+import Container from '../src/Container';
+
+let wrapper;
+
+describe('<Container />', () => {
+  it('renders', () => {
+    wrapper = shallow(<Container />);
+    assert(wrapper.find('.container').length);
+  });
+
+  it('renders children', () => {
+    wrapper = shallow(<Container><p>yes</p></Container>);
+    assert.equal(wrapper.find('.container').children().type(), 'p');
+  });
+});
