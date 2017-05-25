52780d8a46eb4bd05672476ec24fcec030f1b713
Alexander Odell <alextrastero@gmail.com>
2017-05-25 13:12:42 +0200

    Add stats runner to webpack

--- docs/webpack.config.js
+++ docs/webpack.config.js
@@ -4,6 +4,7 @@ const merge = require('webpack-merge');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 const TARGET = process.env.npm_lifecycle_event;
 const BASE_PATH = path.resolve(__dirname);
@@ -57,66 +58,88 @@ const common = {
   devtool: 'eval'
 };
 
-if (TARGET === 'start') {
-  module.exports = merge({
-    entry: [
-      'react-hot-loader/patch',
-      `webpack-dev-server/client?http://localhost:${PORT}`,
-      'webpack/hot/only-dev-server'
-    ],
-    module: {
-      rules: [{
-        test: /\.css$/,
-        loader: 'style-loader!css-loader?sourceMap?convertToAbsoluteUrls'
-      }]
-    },
-    plugins: [
-      new webpack.HotModuleReplacementPlugin(),
-      new webpack.NamedModulesPlugin(),
-      new webpack.NoEmitOnErrorsPlugin()
-    ],
-    devServer: {
-      contentBase: path.resolve(BASE_PATH, 'assets'),
-      historyApiFallback: true,
-      hot: true,
-      inline: true,
-      port: PORT,
-      stats: { colors: true }
-    }
-  }, common);
-} else {
-  module.exports = merge({
-    module: {
-      rules: [{
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: 'css-loader'
-        })
-      }]
-    },
-    plugins: [
-      new ExtractTextPlugin('application.css'),
-      new webpack.DefinePlugin({
-        'process.env': {
-          NODE_ENV: JSON.stringify('production')
-        }
-      }),
-      new webpack.LoaderOptionsPlugin({
-        minimize: true,
-        debug: false
-      }),
-      new webpack.optimize.UglifyJsPlugin({
-        beautify: false,
-        mangle: {
-          screw_ie8: true,
-          keep_fnames: true
-        },
-        compress: {
-          screw_ie8: true
-        },
-        comments: false
+const devConfig = {
+  entry: [
+    'react-hot-loader/patch',
+    `webpack-dev-server/client?http://localhost:${PORT}`,
+    'webpack/hot/only-dev-server'
+  ],
+  module: {
+    rules: [{
+      test: /\.css$/,
+      loader: 'style-loader!css-loader?sourceMap?convertToAbsoluteUrls'
+    }]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  ],
+  devServer: {
+    contentBase: path.resolve(BASE_PATH, 'assets'),
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    port: PORT,
+    stats: { colors: true }
+  }
+};
+
+const prodConfig = {
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: 'css-loader'
       })
-    ]
-  }, common);
+    }]
+  },
+  plugins: [
+    new ExtractTextPlugin('application.css'),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      beautify: false,
+      mangle: {
+        screw_ie8: true,
+        keep_fnames: true
+      },
+      compress: {
+        screw_ie8: true
+      },
+      comments: false
+    })
+  ]
+};
+
+const statsConfig = {
+  plugins: [
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static'
+    })
+  ]
+};
+
+const configs = {
+  start: merge(devConfig, common),
+  stats: merge(prodConfig, statsConfig, common),
+  build: merge(prodConfig, common)
 }
+
+module.exports = configs[TARGET];
+
+// if (TARGET === 'start') {
+//   module.exports = merge(devConfig, common);
+// } else if (TARGET === 'stats') {
+//   module.exports = merge(prodConfig, statsConfig, common);
+// } else {
+//   module.exports = merge(prodConfig, common);
+// }
