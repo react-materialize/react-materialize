5f29c4e7e252899769e395684e43e289c26a2401
Alexander Odell <alextrastero@gmail.com>
2017-05-25 12:07:45 +0200

    Remove createHistory from App

--- docs/src/components/App.js
+++ docs/src/components/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import createBrowserHistory from 'history/createBrowserHistory'
 
 import Navigation from './Navigation';
 import Footer from './Footer';
