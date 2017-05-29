15186962ca798a5f2d6e4d7d0ed4318418033adc
Alexander Odell <alextrastero@gmail.com>
2017-05-29 16:42:20 +0200

    Change paths to hashes (#255)

--- docs/src/components/App.js
+++ docs/src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 import Navigation from './Navigation';
 import Footer from './Footer';
