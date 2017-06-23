fef6e0fa58429a11702d5047b76c0c4a1fd131a0
Alexander Odell <alextrastero@gmail.com>
2017-06-23 09:41:38 +0200

    Adds fixed prop to Navbar (#284)

--- test/NavbarSpec.js
+++ test/NavbarSpec.js
@@ -1,28 +1,39 @@
 /* global describe, it, expect */
 
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import Navbar from '../src/Navbar';
 import NavItem from '../src/NavItem';
 
-let wrapper = shallow(
-  <Navbar brand='logo' right>
-    <NavItem href='get-started.html'>Getting started</NavItem>
-    <NavItem href='components.html'>Components</NavItem>
-  </Navbar>
-);
-
 describe('<Navbar />', () => {
-  it('renders a navbar', () => {
-    expect(wrapper.find('nav')).to.have.length(1);
-  });
+  let wrapper;
 
-  it('adds a brand text', () => {
-    expect(wrapper.find('a.brand-logo').text()).to.equal('logo');
+  it('renders a navbar', () => {
+    wrapper = shallow(
+      <Navbar brand='Logo' right>
+        <NavItem href='get-started.html'>Getting started</NavItem>
+        <NavItem href='components.html'>Components</NavItem>
+      </Navbar>
+    );
+
+    expect(wrapper.find('nav')).to.have.length(1);
   });
 
   it('places brand on right if navbar is left aligned', () => {
     wrapper = shallow(<Navbar brand='logo' left />);
     expect(wrapper.find('a.brand-logo.right')).to.have.length(1);
+  });
+
+  it('adds a brand node', () => {
+    const brandNode = <span className='brando'>I AM BRAND</span>;
+
+    wrapper = mount(<Navbar brand={brandNode} />);
+    expect(wrapper.find('a.brand-logo')
+      .contains(brandNode)).to.equal(true);
+  });
+
+  it('can be fixed', () => {
+    wrapper = shallow(<Navbar fixed />);
+    expect(wrapper.hasClass('navbar-fixed')).to.eq(true);
   });
 });
