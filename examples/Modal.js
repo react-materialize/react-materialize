7eeeb7a6de2faafc0a3410e33574a911abcee42e
Alexander Odell <alextrastero@gmail.com>
2017-07-20 18:57:42 +0200

    Add more examples to ModalsPage

--- examples/Modal.js
+++ examples/Modal.js
@@ -5,8 +5,6 @@ import Button from '../src/Button';
 export default
 <Modal
   header='Modal Header'
-  trigger={
-    <Button waves='light'>MODAL</Button>
-  }>
+  trigger={<Button waves='light'>MODAL</Button>}>
   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
 </Modal>;
