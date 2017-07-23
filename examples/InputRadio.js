3180aaf276268d0513e96babaf16bcdcd6672165
Ruedi Steinmann <ruedi.steinmann@ecamos.ch>
2017-07-23 19:49:06 +0200

    fix radio button documentation

--- examples/InputRadio.js
+++ examples/InputRadio.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Input from '../src/Input';
+import Row from '../src/Row';
 
 export default
-<div>
+<Row>
     <Input name='group1' type='radio' value='red' label='Red' />
     <Input name='group1' type='radio' value='yellow' label='Yellow' />
     <Input name='group1' type='radio' value='green' label='Green' className='with-gap' />
     <Input name='group1' type='radio' value='brown' label='Brown' disabled='disabled' />
-</div>;
+</Row>;
