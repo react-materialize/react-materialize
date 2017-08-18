0f8bc1d5a65fe1d9304e4ae1deb732a1ff3ff0fa
muddydixon <muddydixon@gmail.com>
2017-08-18 14:33:36 +0900

    support card horizontal

--- src/Card.js
+++ src/Card.js
@@ -10,18 +10,25 @@ class Card extends Component {
   }
 
   render () {
-    let {title, header, className, textClassName, actions, reveal, children, ...props} = this.props;
+    let {title, header, className, textClassName, horizontal, actions, reveal, children, ...props} = this.props;
     let classes = { card: true };
-    return (
-      <div {...props}
-        className={cx(className, classes)} >
-        {header}
+
+    let content = (
+      <div>
         <div className={cx('card-content', textClassName)}>
           {title ? this.renderTitle(title, reveal) : null}
           <div>{children}</div>
         </div>
         {this.renderReveal(title, reveal)}
         {actions ? this.renderAction(actions) : null}
+      </div>);
+    if(horizontal) content = <div className="card-stacked">{content}</div>;
+
+    return (
+      <div {...props}
+        className={cx(className, classes, {horizontal})} >
+        {header}
+        {content}
       </div>
     );
   }
