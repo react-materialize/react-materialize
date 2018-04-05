c84b0fe114e9ec23fa2b8c68d1e2d0262776f73d
Alexander Odell <alextrastero@gmail.com>
2018-04-05 22:28:55 +0200

    Add Col.spec

--- test/Col.spec.js
+++ test/Col.spec.js
@@ -1,19 +1,16 @@
-/* global describe, it */
-
 import React from 'react';
 import { shallow } from 'enzyme';
-import { assert } from 'chai';
 import Col from '../src/Col';
 
 let wrapper = shallow(<Col />);
 
 describe('<Col />', () => {
-  test('should render', () => {
-    assert.toBeTruthy();
+  test('renders', () => {
+    expect(wrapper).toMatchSnapshot();
   });
 
   test('accepts sizes as props', () => {
     wrapper = shallow(<Col s={4} m={6} l={8} />);
-    assert(wrapper.find('.col').hasClass('s4 m6 l8'), 'a column');
+    expect(wrapper.find('.col').hasClass('s4 m6 l8')).toEqual(true);
   });
 });
