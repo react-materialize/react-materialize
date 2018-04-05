fb67b901c2500c6723f0dfb5130001da616123b6
Alexander Odell <alextrastero@gmail.com>
2018-04-05 22:42:25 +0200

    Add SearchForm.spec

--- test/SearchForm.spec.js
+++ test/SearchForm.spec.js
@@ -1,14 +1,11 @@
-/* global describe, it */
-
 import React from 'react';
 import { shallow } from 'enzyme';
-import { assert } from 'chai';
 import SearchForm from '../src/SearchForm';
 
 let wrapper = shallow(<SearchForm />);
 
 describe('<SearchForm />', () => {
   test('should render', () => {
-    assert(wrapper.find('form').length);
+    expect(wrapper).toMatchSnapshot();
   });
 });
