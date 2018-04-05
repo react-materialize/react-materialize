6bcff27f826d76f64c0ed7172d9bd63964b54fef
Alexander Odell <alextrastero@gmail.com>
2018-04-05 22:37:48 +0200

    Add MenuItem.spec

--- test/MenuItem.spec.js
+++ test/MenuItem.spec.js
@@ -1,21 +1,15 @@
-/* global describe, it */
-
 import React from 'react';
 import { shallow } from 'enzyme';
-import { assert } from 'chai';
 import MenuItem from '../src/MenuItem';
 
 let wrapper = shallow(<MenuItem className="more" breadcrumbItem href="/" />);
 
 describe('<MenuItem />', () => {
-  test(
-    'should render a link with the className of `breadcrumbItem more`',
-    () => {
-      assert(wrapper.find('a.breadcrumb.more').length);
-    }
-  );
+  test('render', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
 
   test('should render a href if provided', () => {
-    assert.strictEqual(wrapper.props().href, '/');
+    expect(wrapper.props().href).toEqual('/');
   });
 });
