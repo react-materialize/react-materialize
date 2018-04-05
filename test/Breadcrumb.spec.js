8fcc25286f1ccc3cc30c42ddef326106e96eb6c3
Alexander Odell <alextrastero@gmail.com>
2018-04-05 22:16:17 +0200

    Add Breadcrumb.spec

--- test/Breadcrumb.spec.js
+++ test/Breadcrumb.spec.js
@@ -1,14 +1,9 @@
-/* global describe, it */
-
 import React from 'react';
 import { shallow } from 'enzyme';
-import { assert } from 'chai';
 import Breadcrumb from '../src/Breadcrumb';
-
-const wrapper = shallow(<Breadcrumb />);
 
 describe('<Breadcrumb />', () => {
   test('renders', () => {
-    assert(wrapper.find('.breadcrumb'));
+    expect(shallow(<Breadcrumb />)).toMatchSnapshot();
   });
 });
