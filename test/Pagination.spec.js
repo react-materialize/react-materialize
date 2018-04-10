624b2e014a3c94e0a154764de6070adcef54526f
Alexander Odell <alextrastero@gmail.com>
2018-04-10 08:40:19 +0200

    Add Pagination.spec

--- test/Pagination.spec.js
+++ test/Pagination.spec.js
@@ -1,36 +1,34 @@
-/* global describe, it */
-
 import React from 'react';
 import { shallow, mount } from 'enzyme';
-import { assert } from 'chai';
 import Pagination from '../src/Pagination';
 
 describe('<Pagination />', () => {
-  let wrapper = shallow(
-    <Pagination items={10} activePage={2} maxButtons={6} />
-  );
+  let wrapper;
 
-  test('should render', () => {
-    assert(wrapper.find('.pagination').length);
+  beforeEach(() => {
+    wrapper = shallow(<Pagination items={10} activePage={2} maxButtons={6} />);
+  });
+
+  test('renders', () => {
+    expect(wrapper).toMatchSnapshot();
   });
 
   test('has items', () => {
-    assert.strictEqual(wrapper.children().length, 8);
+    expect(wrapper.children()).toHaveLength(8);
   });
 
   test('accepts activePage prop', () => {
-    assert(wrapper.childAt(2).props().active);
+    expect(wrapper.childAt(2).props().active).toBeTruthy();
   });
 
   test('accepts active prop', () => {
     wrapper.setState({ activePage: 4 });
-    assert(wrapper.childAt(4).props().active);
+    expect(wrapper).toMatchSnapshot();
   });
 
   test("doesn't update state with wrong activePage prop", () => {
     wrapper = shallow(<Pagination items={1} activePage={2} />);
-
-    assert.equal(wrapper.state().activePage, 1);
+    expect(wrapper).toMatchSnapshot();
   });
 
   test('updates state with passed activePage prop', () => {
@@ -38,6 +36,6 @@ describe('<Pagination />', () => {
     wrapper = mount(<Pagination items={10} activePage={2} />);
     wrapper.setProps({ activePage: newActive });
 
-    assert.equal(wrapper.state().activePage, newActive);
+    expect(wrapper.state().activePage).toEqual(newActive);
   });
 });
