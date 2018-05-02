c5749c298298352ada47989d89c455df5a7a1de7
Claudio Cortese <claudio@localhost.localdomain>
2018-05-02 17:51:30 +0200

    Code refractored using ES6 classes syntax

--- src/Section.js
+++ src/Section.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-const Section = props => <div className="section">{props.children}</div>;
+class Section extends Component {
+  render() {
+    return <div className="section">{this.props.children}</div>;
+  }
+}
 
 Section.propTypes = {
   children: PropTypes.node
