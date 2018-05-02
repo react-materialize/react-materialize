3aeb99ba9ad4b4ad91080b7c378fed8457ee0c16
Claudio Cortese <claudio.cortese@outlook.it>
2018-05-02 20:32:09 +0200

    It's now possible to add id and customize classNames and node

--- src/Section.js
+++ src/Section.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import cx from 'classnames';
 
 class Section extends Component {
   render() {
-    return <div className="section">{this.props.children}</div>;
+    const { className, node, id, ...other } = this.props;
+
+    let C = node;
+    let classes = {
+      section: true
+    };
+
+    return (
+      <C {...other} id={id} className={cx(classes, className)}>
+        {this.props.children}
+      </C>
+    );
   }
 }
 
 Section.propTypes = {
-  children: PropTypes.node
+  children: PropTypes.node,
+  node: PropTypes.node,
+  id: PropTypes.string,
+  className: PropTypes.string
+};
+
+Section.defaultProps = {
+  node: 'div'
 };
 
 export default Section;
