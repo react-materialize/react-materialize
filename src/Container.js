dce525cb622bf3090a3e17c4e8123646de89339d
Claudio Cortese <claudio.cortese@outlook.it>
2018-09-22 12:37:36 +0200

    Added the possibility to add classes to Container

--- src/Container.js
+++ src/Container.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import cx from 'class-names';
 import PropTypes from 'prop-types';
 
-const Container = props => <div className="container">{props.children}</div>;
+const Container = props => <div className={cx("container", props.className)}>{props.children}</div>;
 
 Container.propTypes = {
-  children: PropTypes.node
+  children: PropTypes.node,
+  className: PropTypes.string
 };
 
 export default Container;
