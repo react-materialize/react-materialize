4c626344cf6db214ca704159ee5288b463e3b9b4
Claudio Cortese <claudio.cortese@outlook.it>
2018-09-22 12:51:03 +0200

    Fixed indentation

--- src/Container.js
+++ src/Container.js
@@ -2,7 +2,9 @@ import React from 'react';
 import cx from 'class-names';
 import PropTypes from 'prop-types';
 
-const Container = props => <div className={cx("container", props.className)}>{props.children}</div>;
+const Container = props => (
+  <div className={cx('container', props.className)}>{props.children}</div>
+);
 
 Container.propTypes = {
   children: PropTypes.node,
