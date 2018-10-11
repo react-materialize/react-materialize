4ade36797b309a1a71d951da886fca422f48f35d
Claudio Cortese <claudio.cortese@outlook.it>
2018-10-11 22:29:40 +0200

    Adding some spec to check correct implementation

--- test/Col.spec.js
+++ test/Col.spec.js
@@ -13,4 +13,14 @@ describe('<Col />', () => {
     wrapper = shallow(<Col s={4} m={6} l={8} />);
     expect(wrapper.find('.col').hasClass('s4 m6 l8')).toEqual(true);
   });
+
+  test('accepts push as props', () => {
+    wrapper = shallow(<Col push="s4" />);
+    expect(wrapper.find('.col').hasClass('push-s4'));
+  });
+
+  test('accepts pull as props', () => {
+    wrapper = shallow(<Col pull="s4" />);
+    expect(wrapper.find('.col').hasClass('pull-s4'));
+  });
 });
