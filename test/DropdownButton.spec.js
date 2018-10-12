f545c0e3246ff95af798bff78c498698416c2967
Claudio Cortese <claudio.cortese@outlook.it>
2018-10-12 19:02:53 +0200

    jest mocking correctly implemented

--- test/DropdownButton.spec.js
+++ test/DropdownButton.spec.js
@@ -2,34 +2,14 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Dropdown from '../src/Dropdown';
 import Divider from '../src/Divider';
-import mocker from './helper/mocker';
+import mocker from './helper/new-mocker';
 
 describe('<Dropdown />', () => {
   let wrapper;
-  const dropdownMock = jest.fn();
-  const restore = mocker('dropdown', dropdownMock);
-
-  afterAll(() => {
-    restore();
-  });
 
   test('renders', () => {
     wrapper = shallow(<Dropdown trigger={<button>Drop me!</button>} />);
     expect(wrapper).toMatchSnapshot();
-  });
-
-  test('handles options prop', () => {
-    const options = { constrainWidth: true };
-    wrapper = shallow(
-      <Dropdown trigger={<button>Drop me!</button>} options={options} />
-    );
-    expect(dropdownMock).toHaveBeenCalledWith(options);
-  });
-
-  test('deletes unwatend props', () => {
-    const options = { hover: true };
-    wrapper = shallow(<Dropdown trigger={<span>hi</span>} options={options} />);
-    expect(wrapper.find('ul').props()).not.toMatchObject(options);
   });
 
   test('does not update ID reference with each render', () => {
@@ -50,5 +30,41 @@ describe('<Dropdown />', () => {
     const { idx } = wrapper.instance();
     wrapper.render();
     expect(wrapper.contains(<li className="divider" tanIndex="-1" />));
+  });
+
+  test('deletes unwatend props', () => {
+    const options = { hover: true };
+    wrapper = shallow(<Dropdown trigger={<span>hi</span>} options={options} />);
+    expect(wrapper.find('ul').props()).not.toMatchObject(options);
+  });
+
+  describe('initializes', () => {
+    const dropdownInitMock = jest.fn();
+    const dropdownInstanceDestroyMock = jest.fn();
+    const dropdownMock = {
+      init: (el, options) => {
+        dropdownInitMock(options);
+        return { destroy: dropdownInstanceDestroyMock };
+      }
+    };
+
+    const restore = mocker('Dropdown', dropdownMock);
+
+    beforeEach(() => {
+      dropdownInitMock.mockClear();
+      dropdownInstanceDestroyMock.mockClear();
+    });
+
+    afterAll(() => {
+      restore();
+    });
+
+    test('handles options prop', () => {
+      const options = { constrainWidth: true };
+      wrapper = shallow(
+        <Dropdown trigger={<button>Drop me!</button>} options={options} />
+      );
+      expect(dropdownInitMock).toHaveBeenCalledWith(options);
+    });
   });
 });
