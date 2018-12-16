cfd84e3396c646d08073973722299b4aa065faa2
Alexander Odell <alextrastero@gmail.com>
2018-12-16 13:02:55 +0100

    Add Select stories

---
+++ stories/Select.stories.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import Select from '../src/Select';
+
+const stories = storiesOf('components/Select', module);
+
+stories.addParameters({
+  info: {
+    text: `Select allows user input through specified options.`
+  }
+});
+
+stories.add('Default', () => (
+  <Select>
+    <option value="" disabled selected>Choose your option</option>
+    <option value="1">Option 1</option>
+    <option value="2">Option 2</option>
+    <option value="3">Option 3</option>
+  </Select>
+));
+
+stories.add('Multiple', () => (
+  <Select multiple>
+    <option value="" disabled selected>Choose your option</option>
+    <option value="1">Option 1</option>
+    <option value="2">Option 2</option>
+    <option value="3">Option 3</option>
+  </Select>
+));
+
+stories.add('Label', () => (
+  <Select label="Choose your option">
+    <option value="1">Option 1</option>
+    <option value="2" selected>Option 2</option>
+    <option value="3">Option 3</option>
+  </Select>
+));
+
+stories.add('Disabled', () => (
+  <Select disabled>
+    <option value="1">Option 1</option>
+    <option value="2" selected>Option 2</option>
+    <option value="3">Option 3</option>
+  </Select>
+));
+
+stories.add('Browser default', () => (
+  <Select browserDefault>
+    <option value="1">Option 1</option>
+    <option value="2" selected>Option 2</option>
+    <option value="3">Option 3</option>
+  </Select>
+));
