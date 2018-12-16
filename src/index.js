2b3a2783bc4ac8923ea7dfdf3bfd3461070f228d
Alexander Odell <alextrastero@gmail.com>
2018-12-16 15:46:04 +0100

    Expose RadioGroup

--- src/index.js
+++ src/index.js
@@ -29,6 +29,7 @@ import Parallax from './Parallax';
 import Preloader from './Preloader';
 import ProgressBar from './ProgressBar';
 import Row from './Row';
+import RadioGroup from './RadioGroup';
 import SearchForm from './SearchForm';
 import Section from './Section';
 import SideNav from './SideNav';
@@ -74,6 +75,7 @@ export {
   Preloader,
   ProgressBar,
   Row,
+  RadioGroup,
   SearchForm,
   Section,
   SideNav,
