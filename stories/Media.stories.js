bc8fd3d45a4dfe106e5d3b09e7fff0e62f0ea616
Luiz Alberto <luiz.alberto1723@gmail.com>
2019-02-18 21:31:48 -0300

    Add media component stories

---
+++ stories/Media.stories.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import MediaBox from '../src/MediaBox';
+import Slider from '../src/Slider';
+import Slide from '../src/Slide';
+
+const stories = storiesOf('javascript/Media', module);
+
+stories.addParameters({
+  info: {
+    text: `Media components include things that have to do with large media objects like Images, Video, Audio, etc.`
+  }
+});
+
+stories.add('Default', () => (
+  <MediaBox src="https://lorempixel.com/350/350/nature/1" caption="A demo media box1"/>
+));
+
+stories.add('Slider', () => (
+  <Slider>
+    <Slide
+      image={<MediaBox src="http://lorempixel.com/780/580/nature/1" caption="A demo media box1"/>}>
+      <div class="caption center-align">
+        <h3>This is our big Tagline!</h3>
+        <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+      </div>
+    </Slide>
+    <Slide
+      image={<MediaBox src="http://lorempixel.com/780/580//nature/2" caption="A demo media box1"/>}>
+      <div class="caption left-align">
+        <h3>Left Aligned Caption</h3>
+        <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+      </div>
+    </Slide>
+    <Slide
+      image={<MediaBox src="http://lorempixel.com/780/580//nature/3" caption="A demo media box1"/>}>
+      <div class="caption right-align">
+        <h3>Right Aligned Caption</h3>
+        <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+      </div>
+    </Slide>
+  </Slider>
+));
+
+
