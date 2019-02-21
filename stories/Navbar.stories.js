b9b1e6e43187a7c3bfa567b7b88455391d99ce36
onluiz <luiz.alberto1723@gmail.com>
2019-02-21 13:14:32 -0300

    Add Navbar stories

---
+++ stories/Navbar.stories.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import Navbar from '../src/Navbar';
+import NavItem from '../src/NavItem';
+import Icon from '../src/Icon';
+
+const stories = storiesOf('components/Navbar', module);
+
+stories.addParameters({
+  info: {
+    text: `The navbar is fully contained by an HTML5 Nav tag. Inside a recommended container div, there are 2 main parts of the navbar. A logo or brand link, and the navigations links. You can align these links to the left or right.`
+  }
+});
+
+stories.add('Default', () => (
+  <Navbar brand={<a href="#" className="brand-logo">Logo</a>} alignLinks="right">
+    <NavItem onClick={() => console.log('test click')}>Getting started</NavItem>
+    <NavItem href='components.html'>Components</NavItem>
+  </Navbar>
+), {info: `To right align your navbar links, just add a  right class to your  <ul> that contains them.`});
+
+stories.add('Left Aligned Links', () => (
+  <Navbar brand={<a href="#" className="brand-logo right">Logo</a>} alignLinks="left">
+    <NavItem onClick={() => console.log('test click')}>Getting started</NavItem>
+    <NavItem href='components.html'>Components</NavItem>
+  </Navbar>
+));
+
+stories.add('Icon Links', () => (
+  <Navbar brand={<a href="#" className="brand-logo">Logo</a>} alignLinks="right">
+    <NavItem href='get-started.html'><Icon>search</Icon></NavItem>
+    <NavItem href='get-started.html'><Icon>view_module</Icon></NavItem>
+    <NavItem href='get-started.html'><Icon>refresh</Icon></NavItem>
+    <NavItem href='get-started.html'><Icon>more_vert</Icon></NavItem>
+  </Navbar>
+));
+
+stories.add('Centering the logo', () => (
+  <Navbar centerLogo brand={<a href="#" className="brand-logo">Logo</a>} alignLinks="left">
+    <NavItem href='get-started.html'><Icon>search</Icon></NavItem>
+    <NavItem href='get-started.html'><Icon>view_module</Icon></NavItem>
+    <NavItem href='get-started.html'><Icon>refresh</Icon></NavItem>
+    <NavItem href='get-started.html'><Icon>more_vert</Icon></NavItem>
+  </Navbar>
+));
+
+stories.add('Active Items', () => (
+  <Navbar centerLogo brand={<a href="#" className="brand-logo">Logo</a>} alignLinks="left">
+    <NavItem href='components.html'>Sass</NavItem>
+    <NavItem href='components.html'>Components</NavItem>
+    <NavItem href='components.html' className="active">React</NavItem>
+  </Navbar>
+));
+
+stories.add('Fixed Navbar', () => (
+  <Navbar fixed brand={<a href="#" className="brand-logo">Logo</a>} alignLinks="right">
+    <NavItem href='components.html'>Sass</NavItem>
+    <NavItem href='components.html'>Components</NavItem>
+    <NavItem href='components.html' className="active">React</NavItem>
+  </Navbar>
+));
