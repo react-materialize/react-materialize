9b0792e990f5c51611f13f9091e14d2b0018563f
onluiz <luiz.alberto1723@gmail.com>
2019-02-23 12:04:04 -0300

    Add pagination stories

---
+++ stories/Pagination.stories.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import Pagination from '../src/Pagination';
+
+const stories = storiesOf('components/Pagination', module);
+
+stories.addParameters({
+  info: {
+    text: `Add pagination links to help split up your long content into shorter, easier to understand blocks. You just have to provide the items and onSelect attribute, when clicked, the onSelect function will be called with the page number. Otherwise you can customize the page button with PaginationButton component.`
+  }
+});
+
+stories.add('Default', () => (
+  <Pagination items={10} activePage={2} maxButtons={8} />
+));
