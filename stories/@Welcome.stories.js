b5f0b80763d6f4f5007c6915ab34d3dc4291954f
Alexander Odell <alextrastero@gmail.com>
2019-03-15 09:58:36 +0100

    Initial welcome story

---
+++ stories/@Welcome.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+const stories = storiesOf('0. React Materialize', module);
+
+stories.add('Welcome', () => (
+  <div>
+    <h1>Hello</h1>
+  </div>
+));
