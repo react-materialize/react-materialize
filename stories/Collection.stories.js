5478979602d07cdfa7e43762feb1857130bec694
Claudio Cortese <claudio.cortese@outlook.it>
2019-04-04 01:05:54 +0200

    feat: add missing Collection stories

--- stories/Collection.stories.js
+++ stories/Collection.stories.js
@@ -4,6 +4,7 @@ import Col from '../src/Col';
 import Row from '../src/Row';
 import Collection from '../src/Collection';
 import CollectionItem from '../src/CollectionItem';
+import Icon from '../src/Icon';
 
 const stories = storiesOf('Components|Collection', module);
 
@@ -47,5 +48,83 @@ stories.add('Header', () => (
     <CollectionItem>Alvin</CollectionItem>
     <CollectionItem>Alvin</CollectionItem>
     <CollectionItem>Alvin</CollectionItem>
+  </Collection>
+));
+
+stories.add('Secondary Content', () => (
+  <Collection header="First Names">
+    <CollectionItem>
+      Alvin
+      <a href="javascript:void(0)" className="secondary-content">
+        <Icon>send</Icon>
+      </a>
+    </CollectionItem>
+    <CollectionItem>
+      Alvin
+      <a href="javascript:void(0)" className="secondary-content">
+        <Icon>send</Icon>
+      </a>
+    </CollectionItem>
+    <CollectionItem>
+      Alvin
+      <a href="javascript:void(0)" className="secondary-content">
+        <Icon>send</Icon>
+      </a>
+    </CollectionItem>
+    <CollectionItem>
+      Alvin
+      <a href="javascript:void(0)" className="secondary-content">
+        <Icon>send</Icon>
+      </a>
+    </CollectionItem>
+  </Collection>
+));
+
+stories.add('Avatar Content', () => (
+  <Collection>
+    <CollectionItem className="avatar">
+      <img
+        src="https://materializecss.com/images/yuna.jpg"
+        alt=""
+        className="circle"
+      />
+      <span className="title">Title</span>
+      <p>
+        First Line <br /> Second Line
+      </p>
+      <a href="javascript:void(0)" className="secondary-content">
+        <Icon>grade</Icon>
+      </a>
+    </CollectionItem>
+    <CollectionItem className="avatar">
+      <Icon className="circle">folder</Icon>
+      <span className="title">Title</span>
+      <p>
+        First Line <br /> Second Line
+      </p>
+      <a href="javascript:void(0)" className="secondary-content">
+        <Icon>grade</Icon>
+      </a>
+    </CollectionItem>
+    <CollectionItem className="avatar">
+      <Icon className="circle green">insert_chart</Icon>
+      <span className="title">Title</span>
+      <p>
+        First Line <br /> Second Line
+      </p>
+      <a href="javascript:void(0)" className="secondary-content">
+        <Icon>grade</Icon>
+      </a>
+    </CollectionItem>
+    <CollectionItem className="avatar">
+      <Icon className="circle red">play_arrow</Icon>
+      <span className="title">Title</span>
+      <p>
+        First Line <br /> Second Line
+      </p>
+      <a href="javascript:void(0)" className="secondary-content">
+        <Icon>grade</Icon>
+      </a>
+    </CollectionItem>
   </Collection>
 ));
