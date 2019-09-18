17eddeb87b3834f851140f19e42a8b5f816ecde5
unknown <aymeric.fiorini@etu.univ-st-etienne.fr>
2019-09-18 10:13:50 +0200

    modified:   src/SearchForm.js

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -3,9 +3,14 @@ import PropTypes from 'prop-types';
 import Icon from './Icon';
 import TextInput from './TextInput';
 
-const SearchForm = ({ searchIcon, closeIcon, onChange, value = "" }) => (
+const SearchForm = ({ searchIcon, closeIcon, onChange, value }) => (
   <form>
-    <TextInput label={searchIcon} type="search" onChange={(e) => onChange(e)} value={value}>
+    <TextInput
+      label={searchIcon}
+      type="search"
+      onChange={e => onChange(e)}
+      value={value}
+    >
       {closeIcon}
     </TextInput>
   </form>
@@ -13,12 +18,16 @@ const SearchForm = ({ searchIcon, closeIcon, onChange, value = "" }) => (
 
 SearchForm.propTypes = {
   searchIcon: PropTypes.node.isRequired,
-  closeIcon: PropTypes.node.isRequired
+  closeIcon: PropTypes.node.isRequired,
+  onChange: PropTypes.func,
+  value: PropTypes.string
 };
 
 SearchForm.defaultProps = {
   searchIcon: <Icon>search</Icon>,
-  closeIcon: <Icon>close</Icon>
+  closeIcon: <Icon>close</Icon>,
+  onChange: null,
+  value: ''
 };
 
 export default SearchForm;
