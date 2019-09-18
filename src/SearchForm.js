47ee069ae629bd2536f1b0e4f4d707003a93e8ba
unknown <aymeric.fiorini@etu.univ-st-etienne.fr>
2019-09-18 08:40:56 +0200

    modified:   SearchForm.js

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -3,9 +3,9 @@ import PropTypes from 'prop-types';
 import Icon from './Icon';
 import TextInput from './TextInput';
 
-const SearchForm = ({ searchIcon, closeIcon }) => (
+const SearchForm = ({ searchIcon, closeIcon, onChange, value = "" }) => (
   <form>
-    <TextInput label={searchIcon} type="search">
+    <TextInput label={searchIcon} type="search" onChange={(e) => onChange(e)} value={value}>
       {closeIcon}
     </TextInput>
   </form>
