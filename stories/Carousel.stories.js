ed5c6cac3f937c998f7bce6756bc04ad6e76947a
Claudio Cortese <claudio.cortese@outlook.it>
2019-11-24 20:37:51 +0100

    test: update stories

--- stories/Carousel.stories.js
+++ stories/Carousel.stories.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Carousel from '../src/Carousel';
+import Button from '../src/Button';
 
 const stories = storiesOf('Javascript|Carousel', module);
 
@@ -47,6 +48,36 @@ stories.add('html content', () => (
       indicators: true
     }}
     className="white-text center"
+  >
+    <div className="red">
+      <h2>First Panel</h2>
+      <p>This is your first panel</p>
+    </div>
+    <div className="amber">
+      <h2>Second Panel</h2>
+      <p>This is your second panel</p>
+    </div>
+    <div className="green">
+      <h2>Third Panel</h2>
+      <p>This is your third panel</p>
+    </div>
+    <div className="blue">
+      <h2>Fourth Panel</h2>
+      <p>This is your fourth panel</p>
+    </div>
+  </Carousel>
+));
+
+stories.add('fixed item', () => (
+  <Carousel
+    options={{
+      fullWidth: true,
+      indicators: true
+    }}
+    className="white-text center"
+    fixedItem={
+      <Button className="white grey-text darken-text-2">BUTTON</Button>
+    }
   >
     <div className="red">
       <h2>First Panel</h2>
