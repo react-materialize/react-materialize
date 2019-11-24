627f212903d140efaa5c31e8f5ff1ae3e3bb780d
Claudio Cortese <claudio.cortese@outlook.it>
2019-11-24 23:38:51 +0100

    refactor: use hooks

--- src/Slider.js
+++ src/Slider.js
@@ -1,80 +1,30 @@
-import React, { Component } from 'react';
+import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 
-class Slider extends Component {
-  constructor(props) {
-    super(props);
-    this.initSlider = this.initSlider.bind(this);
-    this.fullscreenReset = this.fullscreenReset.bind(this);
-    this.setActiveIndex = this.setActiveIndex.bind(this);
-  }
+const Slider = ({ children, className, options, fullscreen, ...props }) => {
+  const _slider = useRef(null);
 
-  initSlider() {
-    if (typeof M !== 'undefined') {
-      this.instance = M.Slider.init(this._slider, this.props.options);
-    }
-  }
+  useEffect(() => {
+    const instance = M.Slider.init(_slider.current, options);
 
-  /**
-   * If the slider was not in fullscreen, the height is set as a style attribute
-   * on the Slider element. When `.destroy()` is called, this attribute is not
-   * removed, resulting in a fullscreen displayed incorrectly.
-   */
-  fullscreenReset(wasFullscreen) {
-    if (!wasFullscreen && this.props.fullscreen) {
-      this.instance.el.removeAttribute('style');
-      this.instance.el.childNodes[0].removeAttribute('style');
-    }
-  }
-
-  setActiveIndex(activeIndex) {
-    let { indicators } = this.props.options;
-    // In case this option is not defined, we assume true, as per default
-    let showIndicators = typeof indicators === 'undefined' || indicators;
-    if (showIndicators && activeIndex) {
-      this.instance['$indicators'][activeIndex].className =
-        'indicator-item active';
-    }
-  }
-
-  componentDidMount() {
-    this.initSlider();
-  }
-
-  componentDidUpdate(prevProps) {
-    if (!this.instance) return;
-    const activeIndex = this.instance.activeIndex;
-    this.instance.destroy();
-    this.fullscreenReset(prevProps.fullscreen);
-    this.initSlider();
-    // keep indicator at current index displayed
-    this.setActiveIndex(activeIndex);
-  }
-
-  componentWillUnmount() {
-    if (this.instance) {
-      this.instance.destroy();
-    }
-  }
-
-  render() {
-    const { fullscreen, children, className } = this.props;
-    const classes = {
-      fullscreen,
-      slider: true
+    return () => {
+      instance.current && instance.current.destroy();
     };
+  }, [_slider]);
 
-    return (
-      <div
-        ref={node => (this._slider = node)}
-        className={cx(classes, className)}
-      >
-        <ul className="slides">{children}</ul>
-      </div>
-    );
-  }
-}
+  return (
+    <div
+      ref={_slider}
+      className={cx('slider', className, {
+        fullscreen
+      })}
+      {...props}
+    >
+      <ul className="slides">{children}</ul>
+    </div>
+  );
+};
 
 Slider.propTypes = {
   className: PropTypes.string,
