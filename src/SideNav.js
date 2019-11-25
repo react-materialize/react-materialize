72694da379cf991e3cfa1bd7492bc0919718cd6b
daubaris <paulius.daubaris@gmail.com>
2019-11-25 09:14:12 +0200

    SideNav: convert to stateless

--- src/SideNav.js
+++ src/SideNav.js
@@ -1,52 +1,38 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment, useEffect, useRef } from 'react';
 import cx from 'classnames';
 import PropTypes from 'prop-types';
+
 import idgen from './idgen';
 
-class SideNav extends Component {
-  constructor(props) {
-    super(props);
-    this.id = props.id || `sidenav_${idgen()}`;
-  }
+const SideNav = ({
+  options,
+  id,
+  className,
+  children,
+  trigger,
+  fixed,
+  ...props
+}) => {
+  const instance = useRef(null);
+  const sidenavRef = useRef(null);
+  const _id = id || `sidenav_${idgen()}`;
+  const classNames = cx(
+    'sidenav',
+    { 'sidenav-fixed': fixed || !trigger },
+    className
+  );
 
-  componentDidMount() {
-    if (typeof M !== 'undefined') {
-      const { options } = this.props;
-      this.instance = M.Sidenav.init(this._sidenav, options);
-    }
-  }
+  useEffect(() => {
+    instance.current = M.Sidenav.init(sidenavRef.current, options);
 
-  componentWillUnmount() {
-    this.instance && this.instance.destroy();
-  }
+    return () => {
+      if (instance.current) {
+        instance.current.destroy();
+      }
+    };
+  }, [sidenavRef.current, instance.current]);
 
-  render() {
-    const { className, children, trigger, fixed, ...props } = this.props;
-    delete props.id;
-    delete props.options;
-    const classNames = cx(
-      'sidenav',
-      { 'sidenav-fixed': fixed || !trigger },
-      className
-    );
-
-    return (
-      <Fragment>
-        {this.renderTrigger()}
-        <ul
-          ref={el => (this._sidenav = el)}
-          id={this.id}
-          className={classNames}
-          {...props}
-        >
-          {children}
-        </ul>
-      </Fragment>
-    );
-  }
-
-  renderTrigger() {
-    const { trigger, fixed } = this.props;
+  const renderTrigger = () => {
     if (!trigger) {
       return;
     }
@@ -57,12 +43,26 @@ class SideNav extends Component {
       'sidenav-trigger'
     );
     return React.cloneElement(trigger, {
-      ref: t => (this._trigger = `[data-target=${this.id}]`),
-      'data-target': this.id,
+      ref: t => (t._trigger = `[data-target=${_id}]`),
+      'data-target': _id,
       className: classNames
     });
-  }
-}
+  };
+
+  return (
+    <Fragment>
+      {renderTrigger()}
+      <ul
+        ref={el => (sidenavRef.current = el)}
+        id={_id}
+        className={classNames}
+        {...props}
+      >
+        {children}
+      </ul>
+    </Fragment>
+  );
+};
 
 SideNav.propTypes = {
   /**
