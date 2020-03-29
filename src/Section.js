e213a0ccd3ef59f768777fffa90ab8b93896195d
Alexander Odell <alextrastero@gmail.com>
2020-03-29 12:44:38 +0200

    Section: refactor to stateless / add type definition

--- src/Section.js
+++ src/Section.js
@@ -2,32 +2,17 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 
-class Section extends Component {
-  render() {
-    const { className, node, id, ...other } = this.props;
+const Section = ({ node = 'div', className, ...rest }) => {
+  const Component = node;
 
-    let C = node;
-    let classes = {
-      section: true
-    };
-
-    return (
-      <C {...other} id={id} className={cx(classes, className)}>
-        {this.props.children}
-      </C>
-    );
-  }
-}
+  return <Component className={cx('section', className)} {...rest} />;
+};
 
 Section.propTypes = {
   children: PropTypes.node,
   node: PropTypes.node,
   id: PropTypes.string,
   className: PropTypes.string
-};
-
-Section.defaultProps = {
-  node: 'div'
 };
 
 export default Section;
--- types/sample.test.tsx
+++ types/sample.test.tsx
@@ -3,11 +3,13 @@ import React from 'react';
 import {
   Autocomplete,
   Tabs,
-  Tab
+  Tab,
+  Section
 } from 'react-materialize';
 
 <>
   <Autocomplete options={{ data: { asd: '32' }, onAutocomplete: () => 32 }} s={3} />;
   <Tabs className="hello" defaultValue="what" options={{ duration: 32, onShow: () => {}, swipeable: true, responsiveThreshold: 32 }} onChange={() => {}} />
   <Tab idx="as" title="Hello" />
+  <Section node="div" className="foo" id="bar" />
 </>
