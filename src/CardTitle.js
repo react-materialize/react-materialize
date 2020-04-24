dc6f56f9a1b71fab3e0c7c7d4773c0033569bf82
Alexander Odell <alextrastero@gmail.com>
2020-04-24 11:57:09 +0200

    CardTitle: fix bug where classname prop was overwriting required classname

--- src/CardTitle.js
+++ src/CardTitle.js
@@ -4,8 +4,8 @@ import cx from 'classnames';
 
 import constants from './constants';
 
-const CardTitle = ({ image, reveal, waves, children, ...props }) => {
-  const classes = cx({
+const CardTitle = ({ className, image, reveal, waves, children, ...props }) => {
+  const classes = cx(className, {
     'card-image': true,
     'waves-effect': waves,
     'waves-block': waves,
@@ -22,12 +22,22 @@ const CardTitle = ({ image, reveal, waves, children, ...props }) => {
 
 CardTitle.propTypes = {
   children: PropTypes.node,
-  // Whether the image serves as activator for the reveal
+  /**
+   * Whether the image serves as activator for the reveal
+   */
   reveal: PropTypes.bool,
-  // the waves effect
+  /**
+   * the waves effect
+   */
   waves: PropTypes.oneOf(constants.WAVES),
-  // The path to the image
-  image: PropTypes.string.isRequired
+  /**
+   * The path to the image
+   */
+  image: PropTypes.string.isRequired,
+  /**
+   * custom classname
+   */
+  className: PropTypes.string
 };
 
 export default CardTitle;
