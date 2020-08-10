766102b55498a8e49eb138bc0bfea46fe1659047
Niels Becker <aaqqws@googlemail.com>
2020-08-10 11:06:04 +0200

    Fix Spinner not exported

--- src/index.js
+++ src/index.js
@@ -39,6 +39,7 @@ export { default as SideNav } from './SideNav';
 export { default as SideNavItem } from './SideNavItem';
 export { default as Slide } from './Slide';
 export { default as Slider } from './Slider';
+export { default as Spinner } from './Spinner';
 export { default as Switch } from './Switch';
 export { default as Tab } from './Tab';
 export { default as Table } from './Table';
