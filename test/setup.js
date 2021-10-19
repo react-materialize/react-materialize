fc715a46abfd88a641bb15d2c924dcd9e98a0078
Alexander Odell <alextrastero@gmail.com>
2021-10-19 11:51:08 +0200

    npm: remove enzyme

--- test/setup.js
+++ test/setup.js
@@ -1,5 +1,3 @@
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
 import idgen from '../src/idgen';
 
 import 'mutationobserver-shim';
@@ -7,5 +5,3 @@ import 'mutationobserver-shim';
 jest.mock('../src/idgen', () => jest.fn(() => 'mockId'));
 
 global.M = require('materialize-css');
-
-Enzyme.configure({ adapter: new Adapter() });
